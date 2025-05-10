ae519156c0267f6e8002f4eb4a8b633464811f3b
Benjamin Chung <ckfinite@gmail.com>
2025-05-10 14:35:26 -0700

    More renaming

--- test/const_resolve.jl
+++ test/const_resolve.jl
@@ -1,8 +1,8 @@
 module ConstResolveTests
-using IDL
+using IDLParser
 using Test, PEG
-import IDL.Parse: const_expr, type_spec, module_dcl
-import IDL.ConstResolution: resolve_constants
+import IDLParser.Parse: const_expr, type_spec, module_dcl
+import IDLParser.ConstResolution: resolve_constants
 
 @testset "Const resolution" begin
 empty_scope = Dict{Symbol, Any}()
@@ -31,20 +31,20 @@ empty_scope = Dict{Symbol, Any}()
 @test resolve_constants(parse_whole(const_expr, "2-hello::world"), Dict{Symbol, Any}(:hello => Dict{Symbol, Any}(:world => 2)), empty_scope) == 0
 
 
-@test resolve_constants(parse_whole(type_spec, "string<32*2>"), empty_scope, empty_scope) == IDL.ConstResolution.TypeSpec.TString(64)
-@test resolve_constants(parse_whole(type_spec, "wstring<32*2>"), empty_scope, empty_scope) == IDL.ConstResolution.TypeSpec.TWString(64)
-@test resolve_constants(parse_whole(type_spec, "sequence<int16,3+3>"), empty_scope, empty_scope) == IDL.ConstResolution.TypeSpec.TSeq(IDL.ConstResolution.TypeSpec.TInt(16), 6)
-@test resolve_constants(parse_whole(type_spec, "fixed<32*2, 12>"), empty_scope, empty_scope) == IDL.ConstResolution.TypeSpec.TSpecifiedFixedPoint(64,12)
-@test resolve_constants(parse_whole(type_spec, "map<int16, int16, 64>"), empty_scope, empty_scope) == IDL.ConstResolution.TypeSpec.TMap(IDL.ConstResolution.TypeSpec.TInt(16), IDL.ConstResolution.TypeSpec.TInt(16), 64)
+@test resolve_constants(parse_whole(type_spec, "string<32*2>"), empty_scope, empty_scope) == IDLParser.ConstResolution.TypeSpec.TString(64)
+@test resolve_constants(parse_whole(type_spec, "wstring<32*2>"), empty_scope, empty_scope) == IDLParser.ConstResolution.TypeSpec.TWString(64)
+@test resolve_constants(parse_whole(type_spec, "sequence<int16,3+3>"), empty_scope, empty_scope) == IDLParser.ConstResolution.TypeSpec.TSeq(IDLParser.ConstResolution.TypeSpec.TInt(16), 6)
+@test resolve_constants(parse_whole(type_spec, "fixed<32*2, 12>"), empty_scope, empty_scope) == IDLParser.ConstResolution.TypeSpec.TSpecifiedFixedPoint(64,12)
+@test resolve_constants(parse_whole(type_spec, "map<int16, int16, 64>"), empty_scope, empty_scope) == IDLParser.ConstResolution.TypeSpec.TMap(IDLParser.ConstResolution.TypeSpec.TInt(16), IDLParser.ConstResolution.TypeSpec.TInt(16), 64)
 
 resolve_constants(parse_whole(module_dcl, """
 module M {
     const uint32 A = 3;
     const uint32 B = A;
 }
-"""), Dict{Symbol, Any}(), Dict{Symbol, Any}()) == IDL.ConstResolution.ModuleDecl.MDecl(:M, [
-    IDL.ConstResolution.ConstDecl.CDecl(IDL.ConstResolution.TypeSpec.TUInt(32), :A, 3), 
-    IDL.ConstResolution.ConstDecl.CDecl(IDL.ConstResolution.TypeSpec.TUInt(32), :B, 3)])
+"""), Dict{Symbol, Any}(), Dict{Symbol, Any}()) == IDLParser.ConstResolution.ModuleDecl.MDecl(:M, [
+    IDLParser.ConstResolution.ConstDecl.CDecl(IDLParser.ConstResolution.TypeSpec.TUInt(32), :A, 3), 
+    IDLParser.ConstResolution.ConstDecl.CDecl(IDLParser.ConstResolution.TypeSpec.TUInt(32), :B, 3)])
 
     resolve_constants(parse_whole(module_dcl, """
     module M {
@@ -53,10 +53,10 @@ module M {
         };
         const uint32 B = D::A;
     }
-    """), Dict{Symbol, Any}(), Dict{Symbol, Any}()) == IDL.ConstResolution.ModuleDecl.MDecl(:M, [
-        IDL.ConstResolution.ModuleDecl.MDecl(:D, [
-            IDL.ConstResolution.ConstDecl.CDecl(IDL.ConstResolution.TypeSpec.TUInt(32), :A, 3)]), 
-        IDL.ConstResolution.ConstDecl.CDecl(IDL.ConstResolution.TypeSpec.TUInt(32), :B, 3)])
+    """), Dict{Symbol, Any}(), Dict{Symbol, Any}()) == IDLParser.ConstResolution.ModuleDecl.MDecl(:M, [
+        IDLParser.ConstResolution.ModuleDecl.MDecl(:D, [
+            IDLParser.ConstResolution.ConstDecl.CDecl(IDLParser.ConstResolution.TypeSpec.TUInt(32), :A, 3)]), 
+        IDLParser.ConstResolution.ConstDecl.CDecl(IDLParser.ConstResolution.TypeSpec.TUInt(32), :B, 3)])
 
 ctx = Dict{Symbol, Any}()
 resolve_constants(parse_whole(module_dcl, """
@@ -66,10 +66,10 @@ module M {
     };
     const uint32 B = ::M::D::A;
 }
-"""), ctx, ctx) == IDL.ConstResolution.ModuleDecl.MDecl(:M, [
-    IDL.ConstResolution.ModuleDecl.MDecl(:D, [
-        IDL.ConstResolution.ConstDecl.CDecl(IDL.ConstResolution.TypeSpec.TUInt(32), :A, 3)]), 
-    IDL.ConstResolution.ConstDecl.CDecl(IDL.ConstResolution.TypeSpec.TUInt(32), :B, 3)])
+"""), ctx, ctx) == IDLParser.ConstResolution.ModuleDecl.MDecl(:M, [
+    IDLParser.ConstResolution.ModuleDecl.MDecl(:D, [
+        IDLParser.ConstResolution.ConstDecl.CDecl(IDLParser.ConstResolution.TypeSpec.TUInt(32), :A, 3)]), 
+    IDLParser.ConstResolution.ConstDecl.CDecl(IDLParser.ConstResolution.TypeSpec.TUInt(32), :B, 3)])
 
 end
 end
